b4b7bae22d133cfd15a07a08b656ccd768de56cf
parigya09 <134229557+parigya09@users.noreply.github.com>
2023-08-06 01:38:40 +0800

    managed to make the entire frontend for filter feature in homepage work
    so whenever any button in any category in filter popup is clicked, the background of that button will turn purple and and that clicked data will be saved in a dictionary form to send to backend

--- front-end/src/pages/11_homepage.js
+++ front-end/src/pages/11_homepage.js
@@ -3,7 +3,28 @@ import styles from "./11_homepage.module.css";
 
 const HomePage = () => {
   const [pictures, setPictures] = useState([]); 
-  const [showFilterPopup, setShowFilterPopup] = useState(false); // Step 1: State to control the filter pop-up
+  const [showFilterPopup, setShowFilterPopup] = useState(false);
+  const [selectedItems, setSelectedItems] = useState({
+    "type of clothing": null,
+    "occasion": null,
+    "geography": null,
+  });
+
+  const onItemClick = useCallback((category, item) => {
+    setSelectedItems((prevSelectedItems) => {
+      // Toggle selection: If the item is already selected, set it to null; otherwise, set it to the clicked item
+      const newItemValue = prevSelectedItems[category] === item ? null : item;
+      
+      // Return the updated state
+      return { ...prevSelectedItems, [category]: newItemValue };
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log("Selected Items:", selectedItems);
+  }, [selectedItems]);
+
+
 
   useEffect(() => { 
     // Fetch pictures from the backend here and update the state with the data
@@ -24,97 +45,7 @@ const HomePage = () => {
     ];
     setPictures(fetchedPictures);
   }, []); 
-
-  const onGroupContainer3Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer4Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer5Click = useCallback(() => {
-    // Please sync "onboarding - search filter" to the project
-  }, []);
-
-  const onGroupContainer6Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer7Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer8Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer9Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer10Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer11Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer12Click = useCallback(() => {
-    // Please sync "onboarding - search filter" to the project
-  }, []);
-
-  const onGroupContainer13Click = useCallback(() => {
-    // Please sync "onboarding - search filter" to the project
-  }, []);
-
-  const onGroupContainer14Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer16Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer18Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer19Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer20Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer23Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer24Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer25Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []);
-
-  const onGroupContainer26Click = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []); 
-
-  const onapplyFiltersClick = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []); 
-
-  const onresetFiltersClick = useCallback(() => {
-    // Please sync "warning - no data present" to the project
-  }, []); 
-
-
-    
+  
   const toggleFilterPopup = () => {
     // Step 2: Function to toggle the filter pop-up state
     setShowFilterPopup((prevValue) => !prevValue);
@@ -151,112 +82,113 @@ const HomePage = () => {
           <div className={styles.occasion}>Occasion</div>
           <div className={styles.rectangleParent}> 
             <div className={styles.groupChild} />
-            <div className={styles.rectangleGroup} onClick={onGroupContainer3Click}>
+            <div className={`${styles.rectangleGroup} ${selectedItems["type of clothing"] === "Camis" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Camis")}>
               <div className={styles.groupItem} /> 
               <div className={styles.camis}>Camis</div> 
             </div> 
             <div className={styles.searchFilter1}>Search Filter</div>
             <div className={styles.typeOfClothing}>Type Of Clothing</div>
-            <div className={styles.rectangleContainer} onClick={onGroupContainer4Click}> 
+            <div className={`${styles.rectangleContainer} ${selectedItems["type of clothing"] === "Tees" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Tees")}> 
               <div className={styles.groupItem} /> 
               <div className={styles.tees}>Tees</div> 
             </div>
-            <div className={styles.groupDiv} onClick={onGroupContainer5Click}>
-              <div className={styles.groupItem} />
+            <div className={`${styles.groupDiv} ${selectedItems["type of clothing"] === "Shirts" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Shirts")}>
+              <div className={styles.groupItem} /> 
               <div className={styles.shirts}>Shirts</div>
             </div> 
-            <div className={styles.rectangleParent1} onClick={onGroupContainer6Click}> 
+            <div className={`${styles.rectangleParent1} ${selectedItems["type of clothing"] === "Shorts" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Shorts")}> 
               <div className={styles.groupItem} />
               <div className={styles.camis}>Shorts</div> 
             </div>
-            <div className={styles.rectangleParent2} onClick={onGroupContainer7Click}> 
+            <div className={`${styles.rectangleParent2} ${selectedItems["type of clothing"] === "Skirts" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Skirts")}> 
               <div className={styles.groupItem} />
-              <div className={styles.skirts}>Skirts</div>
+              <div className={styles.skirts}>Skirts</div> 
             </div> 
-            <div className={styles.rectangleParent3} onClick={onGroupContainer8Click}> 
+            <div className={`${styles.rectangleParent3} ${selectedItems["type of clothing"] === "Jackets" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Jackets")}> 
               <div className={styles.groupItem} />
               <div className={styles.jackets}>Jackets</div> 
             </div> 
-            <div className={styles.rectangleParent4} onClick={onGroupContainer9Click}> 
+            <div className={`${styles.rectangleParent4} ${selectedItems["type of clothing"] === "Cardigans" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Cardigans")}>  
               <div className={styles.groupItem} />
               <div className={styles.cardigans}>Cardigans</div> 
             </div> 
-            <div className={styles.rectangleParent5} onClick={onGroupContainer10Click}> 
+            <div className={`${styles.rectangleParent5} ${selectedItems["type of clothing"] === "Hoodies" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Hoodies")}>  
               <div className={styles.groupItem} />
               <div className={styles.hoodies}>Hoodies</div> 
             </div> 
-            <div className={styles.rectangleParent6} onClick={onGroupContainer11Click}> 
+            <div className={`${styles.rectangleParent6} ${selectedItems["type of clothing"] === "Tank Tops" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Tank Tops")}>  
               <div className={styles.groupItem} />
               <div className={styles.tankTops}>Tank Tops</div> 
             </div> 
-            <div className={styles.rectangleParent7} onClick={onGroupContainer12Click}> 
+            <div className={`${styles.rectangleParent7} ${selectedItems["type of clothing"] === "Dresses" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Dresses")}> 
               <div className={styles.groupItem} />
               <div className={styles.dresses}>Dresses</div> 
             </div> 
-            <div className={styles.rectangleParent8} onClick={onGroupContainer13Click}> 
+            <div className={`${styles.rectangleParent8} ${selectedItems["type of clothing"] === "Crop Tops" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Crop Tops")}>  
               <div className={styles.groupItem} />
               <div className={styles.cardigans}>Crop Tops</div> 
             </div> 
-            <div className={styles.rectangleParent9} onClick={onGroupContainer14Click}> 
+            <div className={`${styles.rectangleParent9} ${selectedItems["type of clothing"] === "Pants" ? styles.active : ""}`} onClick={() => onItemClick("type of clothing", "Pants")}> 
               <div className={styles.groupItem} />
               <div className={styles.skirts}>Pants</div> 
             </div> 
+
             <div className={styles.geography}>Geography</div> 
-            <div className={styles.rectangleParent10}>
+            <div className={`${styles.rectangleParent10} ${selectedItems["geography"] === "Korea" ? styles.active : ""}`} onClick={() => onItemClick("geography", "Korea")}>
               <div className={styles.groupChild11} />
               <div className={styles.shirts}>Korea</div> 
             </div> 
-            <div className={styles.rectangleParent11} onClick={onGroupContainer16Click}> 
+            <div className={`${styles.rectangleParent11} ${selectedItems["geography"] === "Japan" ? styles.active : ""}`} onClick={() => onItemClick("geography", "Japan")}> 
               <div className={styles.groupChild11} />
               <div className={styles.shirts}>Japan</div> 
             </div> 
-            <div className={styles.rectangleParent12}>
+            <div className={`${styles.rectangleParent12} ${selectedItems["geography"] === "Western" ? styles.active : ""}`} onClick={() => onItemClick("geography", "Western")}>
               <div className={styles.groupChild11} />
               <div className={styles.western}>Western</div> 
             </div> 
-            <div className={styles.rectangleParent13} onClick={onGroupContainer18Click}>
+            <div className={`${styles.rectangleParent13} ${selectedItems["geography"] === "China" ? styles.active : ""}`} onClick={() => onItemClick("geography", "China")}>
               <div className={styles.groupChild11} />
               <div className={styles.shirts}>China</div> 
             </div> 
 
-            <div className={styles.rectangleParent14} onClick={onGroupContainer19Click}>
+            <div className={`${styles.rectangleParent14} ${selectedItems["occasion"] === "Casual" ? styles.active : ""}`} onClick={() => onItemClick("occasion", "Casual")}>
               <div className={styles.groupChild11} />
               <div className={styles.casual}>Casual</div> 
             </div> 
-            <div className={styles.rectangleParent15} onClick={onGroupContainer20Click}>
+            <div className={`${styles.rectangleParent15} ${selectedItems["occasion"] === "Wedding" ? styles.active : ""}`} onClick={() => onItemClick("occasion", "Wedding")}>
               <div className={styles.groupChild11} />
               <div className={styles.wedding}>Wedding</div> 
             </div> 
-            <div className={styles.rectangleParent16}>
+            <div className={`${styles.rectangleParent16} ${selectedItems["occasion"] === "Formal" ? styles.active : ""}`} onClick={() => onItemClick("occasion", "Formal")}>
               <div className={styles.groupChild11} />
               <div className={styles.casual}>Formal</div> 
             </div> 
-            <div className={styles.rectangleParent17}>
+            <div className={`${styles.rectangleParent17} ${selectedItems["occasion"] === "Brunch" ? styles.active : ""}`} onClick={() => onItemClick("occasion", "Brunch")}>
               <div className={styles.groupChild11} />
               <div className={styles.jackets}>Brunch</div> 
             </div> 
-            <div className={styles.rectangleParent18} onClick={onGroupContainer23Click}>
+            <div className={`${styles.rectangleParent18} ${selectedItems["occasion"] === "Christmas" ? styles.active : ""}`} onClick={() => onItemClick("occasion", "Christmas")}>
               <div className={styles.groupChild11} />
               <div className={styles.cardigans}>Christmas</div> 
             </div> 
-            <div className={styles.rectangleParent19} onClick={onGroupContainer24Click}>
+            <div className={`${styles.rectangleParent19} ${selectedItems["occasion"] === "New Year" ? styles.active : ""}`} onClick={() => onItemClick("occasion", "New Year")}>
               <div className={styles.groupChild11} />
               <div className={styles.tankTops}>New Year</div> 
             </div> 
-            <div className={styles.rectangleParent20} onClick={onGroupContainer25Click}>
+            <div className={`${styles.rectangleParent20} ${selectedItems["occasion"] === "Beach" ? styles.active : ""}`} onClick={() => onItemClick("occasion", "Beach")}>
               <div className={styles.groupChild11} />
-              <div className={styles.camis}>Beach</div> 
-            </div> 
-            <div className={styles.rectangleParent21} onClick={onGroupContainer26Click}>
+              <div className={styles.camis}>Beach</div>
+            </div>
+            <div className={`${styles.rectangleParent21} ${selectedItems["occasion"] === "Birthday" ? styles.active : ""}`} onClick={() => onItemClick("occasion", "Birthday")}>
               <div className={styles.groupChild11} />
               <div className={styles.western}>Birthday</div> 
             </div> 
 
-            <div className={styles.rectangleParent22} onClick={onapplyFiltersClick}> 
+            <div className={styles.rectangleParent22}> 
               <div className={styles.groupChild23} /> 
               <div className={styles.applyFilters}>Apply Filters</div> 
             </div> 
-            <div className={styles.rectangleParent23} onClick={onresetFiltersClick}>
+            <div className={styles.rectangleParent23}>
               <div className={styles.groupChild24} /> 
               <div className={styles.applyFilters}>Reset Filters</div> 
             </div> 
