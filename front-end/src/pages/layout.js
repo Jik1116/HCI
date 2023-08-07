88969a8725e1abc2ca6dd79f8b1ef4da44357e4e
Jik1116 <jaikirat1116@gmail.com>
2023-08-07 08:24:46 +0800

    depo issue

--- front-end/src/pages/layout.js
+++ front-end/src/pages/layout.js
@@ -1,22 +1,17 @@
+// layout.js
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'SparkStyle',
-  description: 'Catered fashion',
-};
+// Remove the export statement for metadata
+// export const metadata = {
+//   title: 'SparkStyle',
+//   description: 'Catered fashion',
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-w idth, initial-scale=1.0" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Add any other meta tags or links to external stylesheets here */}
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
